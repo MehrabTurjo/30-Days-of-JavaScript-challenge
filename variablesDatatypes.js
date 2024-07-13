de38f2e88182364dfa66adf7a0ee3df4540d195a
MehrabTurjo <mehrabturjo2001@gmail.com>
2024-07-14 05:18:21 +0600

    first day of challenge

---
+++ variablesDatatypes.js
@@ -0,0 +1,44 @@
+// Task 1
+
+var pos = 10;
+console.log("This is from var",pos);
+
+// Task 2 
+
+let high = 20;
+console.log("This is from let",high);
+
+// Task 3
+
+const boolean = true;
+console.log("This value is from boolean", boolean);
+
+// Task 4 
+
+const arr = [3,4,5,6];
+const str = "How are you doing?";
+const obj = {
+    name: "Meharb",
+    job: "Unemployed",
+    Age: 10,
+}
+const num = 100;
+
+console.log(arr, typeof(arr));
+console.log(str, typeof(str));
+console.log(obj, typeof(obj));
+console.log(num, typeof(num));
+
+// Task 5 
+
+let first = 100;
+console.log(first);
+
+first = 200;
+console.log(first);
+
+// Task 6
+ 
+const second = 300;
+//second = 200;
+
