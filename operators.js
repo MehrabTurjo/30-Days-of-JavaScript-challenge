32f934f945d2c56a924dfb9d1b192db65f24a0e2
Mehrab Hossain <96405981+MehrabTurjo@users.noreply.github.com>
2024-07-14 23:00:05 +0600

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,71 @@
+// Activity 1
+let a = 10;
+let b = 20; 
+console.log("a = 10 and b = 20") 
+console.log("sum is", a+b);      //Task 1
+console.log("Subract is", a-b);  //Task 2
+console.log("Multiply is", a*b); //Task 3
+console.log("Divide is", a/b);   //Task 4
+console.log("Remainder is", a%b);//Task 5
+ 
+// Activity 2
+let sum =  10;
+sum += 10;
+console.log(sum); // Task 6
+sum -= 10;
+console.log(sum); // Task 7
+
+// Activity 3
+// Task 8
+a > b ? console.log('a greater than b', true) : console.log(false);
+a < b ? console.log('a less than b', true) : console.log(false);
+
+// Task 9
+a <= b ? console.log('a greater or equal to b', true) : console.log(false)
+a >= b ? console.log('a less than or equal to b', true) : console.log(false)
+
+// Task 10
+a = "10";
+b = 10;
+
+if(a===b){
+    console.log("value and type of a and b is same ");
+}
+else if (a == b){
+    console.log("value of a = 10, and b = 10 is equal");
+}
+else{
+    console.log("a and b are not equal");
+}
+// Task 11
+if (a == b && a === b)
+{
+    console.log("value and type of a and b is same ")
+}
+else if(a==b) {
+    console.log("The values are same")
+}
+else{
+    console.log("They are different")
+}
+
+// Task 12
+if (a == b || a === b)
+    {
+        console.log("value or type of a and b is same ")
+    }
+    else if(a==b) {
+        console.log("The values are same")
+    }
+    else{
+        console.log("They are different")
+    }
+// Task 13
+tree = true;
+console.log(tree)   
+if( a == b ) console.log(!tree);
+
+// Activity 5
+
+// Task 14
+a > 0 ? console.log("a is positive") : console.log("a is negative")
